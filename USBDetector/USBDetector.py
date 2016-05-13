9370278ea80ea6812b6650a73a8acaadfb55bab9
Sam Boling <charles.samuel.boling@gmail.com>
2016-05-13 15:44:33 -0400

    mock inst manager tests pass

--- USBDetector/USBDetector.py
+++ USBDetector/USBDetector.py
@@ -1,3 +1,4 @@
+from nameko.rpc import RpcProxy
 from nameko.extensions import DependencyProvider
 from nameko.web.websocket import WebSocketHubProvider, rpc
 
@@ -12,36 +13,17 @@ class ContainerIdentifier(DependencyProvider):
         return id(self.container)
 
 
-class InstrumentManagerProvider(DependencyProvider):
-    class InstrumentCache:
-        def __init__(self):
-            self.instruments = {}
-
-        def add(self, port_info):
-            self.instruments[port_info['deviceName']] = port_info
-
-        def remove(self, port_info):
-            del self.instruments[port_info['deviceName']]
-
-    class InstrumentManager:
-        def __init__(self):
-            self.instruments = self.InstrumentCache()
-
-    def get_dependency(self, worker_ctx):
-        return self.InstrumentManager
-
-
 class Service:
     name = 'USBDetector'
 
     config = Config()
     websocket_hub = WebSocketHubProvider()
-    instrument_mgr = InstrumentManagerProvider()
+    instruments = RpcProxy('instruments')
 
     @rpc
     def add_device(self, socket_id, **port_info):
-        return self.instrument_mgr.instruments.add(port_info)
+        return self.instruments.add(port_info)
 
     @rpc
     def remove_device(self, socket_id, **port_info):
-        return self.instrument_mgr.instruments.remove(port_info)
+        return self.instruments.remove(port_info)
--- USBDetector/tests/test_USBDetector.py
+++ USBDetector/tests/test_USBDetector.py
@@ -7,35 +7,16 @@ from USBDetector.USBDetector import Service
 
 
 @pytest.fixture
-def instrument_mgr():
-    class InstrumentCache:
-        def __init__(self):
-            self.instruments = {}
-
-        def add(self, port_info):
-            self.instruments[port_info['deviceName']] = port_info
-
-        def remove(self, port_info):
-            del self.instruments[port_info['deviceName']]
-
-    class InstrumentManager:
-        def __init__(self):
-            self.instruments = InstrumentCache()
-
-    return InstrumentManager
-
-
-@pytest.fixture
-def websocket_client():
+def websocket_client(websocket):
     class Client:
-        def __init__(self, websocket):
+        def __init__(self):
             self.ws = websocket()
 
         def add(self, name):
-            self.ws.rpc('add_device', deviceName=name)
+            return self.ws.rpc('add_device', deviceName=name)
 
-        async def remove(self, name):
-            self.ws.rpc('remove_device', deviceName=name)
+        def remove(self, name):
+            return self.ws.rpc('remove_device', deviceName=name)
 
     return Client
 
@@ -43,25 +24,28 @@ def websocket_client():
 @pytest.yield_fixture
 def container(container_factory, web_config):
     container = container_factory(Service, web_config)
-    container.start()
     yield container
 
 
 class TestUSBDetector:
     def test_connect(self, container, websocket):
+        container.start()
         websocket()
 
-    # @pytest.mark.asyncio
-    # async def test_add_remove(self, container, instrument_mgr, websocket):
-    #     replace_dependencies(container,
-    #     self.service = worker_factory(
-    #         Service,
-    #         instrument_mgr=instrument_mgr
-    #     )
+    def test_add_remove(self, container, websocket_client):
+        _instruments = {}
 
-    #     async with self.client as client:
-    #         client.add('COM3')
-    #         client.add('COM6')
-    #         client.remove('COM3')
-    #         client.add('COM8')
-    #     assert len(self.service.instrument_mgr.instruments) == 2
+        mock_mgr = replace_dependencies(container, 'instruments')
+        mock_mgr.add.side_effect = lambda dev: _instruments.setdefault(dev['deviceName'], {})
+        mock_mgr.add.return_value = 'added'
+        mock_mgr.remove.side_effect = lambda dev: _instruments.pop(dev['deviceName'])
+        mock_mgr.remove.return_value = 'removed'
+
+        container.start()
+
+        client = websocket_client()
+        client.add('COM3')
+        client.add('COM6')
+        client.remove('COM3')
+        client.add('COM8')
+        assert len(_instruments) == 2
