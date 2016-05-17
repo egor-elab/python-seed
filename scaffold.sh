d17d5afa2b8f559c885588ec4baf3076d340c338
Sam Boling <charles.samuel.boling@gmail.com>
2016-05-17 13:34:03 -0400

    fixing development helpers in scaffold.sh

--- scaffold.sh
+++ scaffold.sh
@@ -2,9 +2,12 @@
 python -m cogapp -r -D service_name=${1:-seed} @template_files
 cp -r seed/ ${1:-auto}
 for f in $(find ${1:-auto} -name '*seed*'); do mv $f ${f/seed/$1}; done
-mkvirtualenv ${1}
-workon .
+
+source ./venv
+workon . || mkvirtualenv ${1}
 pip install -e .
 pip install pytest
 deactivate
 workon .
+
+git remote rm origin
