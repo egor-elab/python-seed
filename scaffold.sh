0fdfd002fdee3982e4fb9177000a749dbc175d33
Sam Boling <charles.samuel.boling@gmail.com>
2016-05-17 13:30:35 -0400

    cog prep script, inherit from base service class

--- scaffold.sh
+++ scaffold.sh
@@ -2,3 +2,9 @@
 python -m cogapp -r -D service_name=${1:-seed} @template_files
 cp -r seed/ ${1:-auto}
 for f in $(find ${1:-auto} -name '*seed*'); do mv $f ${f/seed/$1}; done
+mkvirtualenv ${1}
+workon .
+pip install -e .
+pip install pytest
+deactivate
+workon .
--- seed/Service.py
+++ seed/Service.py
@@ -1,17 +1,11 @@
 from nameko.extensions import DependencyProvider
 
-
-class Config(DependencyProvider):
-    def get_dependency(self, worker_ctx):
-        return self.container.config
+from egor.service.templates import BaseService
 
 
-class Service:
+class Service(BaseService):
     #[[[cog
     #   import cog
     #   cog.outl("name = '{}'".format(service_name))
     #]]]
-    name = "scaffolded"
     #[[[end]]]
-
-    config = Config()
