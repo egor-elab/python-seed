d62e6a85d76f300fe1416fcfaa472582533fffb2
Sam Boling <charles.samuel.boling@gmail.com>
2016-05-17 14:15:24 -0400

    point to github egor as a dependency

--- setup.py
+++ setup.py
@@ -4,11 +4,14 @@ setup(
     #[[[cog
     #   cog.outl("name='{}',".format(service_name))
     #]]]
-    name="scaffolded",
     #[[[end]]]
     packages=find_packages(),
     install_requires=[
         'nameko',
+        'egor',
+    ],
+    dependency_links = [
+        'https://github.com/egor-elab/egor/tarball/master#egg=egor'
     ],
     tests_require=['tox'],
 )
